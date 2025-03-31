01adb634e5e7959e8abe2e57cd5c9305b4fc56fe
osteriux <pixave73@gmail.com>
2025-03-31 15:09:06 +0200

    env setup

--- src/app/info-entreprise/info-entreprise.component.ts
+++ src/app/info-entreprise/info-entreprise.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-info-entreprise',
@@ -11,10 +12,13 @@ import { CommonModule } from '@angular/common';
 export class InfoEntrepriseComponent implements OnInit {
   entreprises: any[] = [];
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    console.log('InfoEntrepriseComponent initialized');
+    console.log('Environment:', environment);
+  }
 
   ngOnInit(): void {
-    this.http.get('/api/entreprises/all').subscribe({
+    this.http.get(environment.BASE_API_URL + '/api/entreprises/all').subscribe({
       next: (data: any) => {
         this.entreprises = data;
         console.log(this.entreprises);
